b6a08b729de59443c6719b130856f6547fb61efe
Canario0 <prenero4bc@gmail.com>
2022-09-18 17:40:19 +0200

    refactor: post cart using command bus

--- src/apps/cart/back/controllers/cartsPostController.ts
+++ src/apps/cart/back/controllers/cartsPostController.ts
@@ -1,20 +1,24 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
-import CartCreate from "../../../../contexts/cartsMs/cart/application/create/cartCreate";
+import CartCreateCommand from "../../../../contexts/cartsMs/cart/application/create/cartCreateCommand";
 import CartAlreadyExists from "../../../../contexts/cartsMs/cart/domain/cartAlreadyExists";
+import CommandBus from "../../../../contexts/shared/domain/commandBus/commandBus";
 import InvalidArgumentError from "../../../../contexts/shared/domain/invalidArgumentError";
 import Logger from "../../../../contexts/shared/domain/logger";
 import Uuid from "../../../../contexts/shared/domain/valueObject/uuid";
 import Controller from "./controllers";
 
 export default class CartsPostController implements Controller {
-  constructor(private cartCreate: CartCreate, private logger: Logger) {}
+  constructor(private commandBus: CommandBus, private logger: Logger) {}
 
   async run(req: Request, res: Response) {
     try {
       const { userId } = req.body;
       const cartId = Uuid.random().toString();
-      await this.cartCreate.run(cartId, userId);
+
+      const command = new CartCreateCommand(cartId, userId);
+      await this.commandBus.dispatch(command);
+
       res.status(httpStatus.CREATED).send({ id: cartId });
     } catch (err) {
       if (err instanceof InvalidArgumentError) {
--- src/tests/contexts/cartsMs/cart/application/create/cartCreateCommandHandler.test.ts
+++ src/tests/contexts/cartsMs/cart/application/create/cartCreateCommandHandler.test.ts
@@ -1,28 +1,37 @@
 import CartCreate from "../../../../../../contexts/cartsMs/cart/application/create/cartCreate";
+import CartCreateCommand from "../../../../../../contexts/cartsMs/cart/application/create/cartCreateCommand";
+import CartCreateCommandHandler from "../../../../../../contexts/cartsMs/cart/application/create/cartCreateCommandHandler";
 import Cart from "../../../../../../contexts/cartsMs/cart/domain/cart";
 import CartAlreadyExists from "../../../../../../contexts/cartsMs/cart/domain/cartAlreadyExists";
 import CartCreatedDomainEvent from "../../../../../../contexts/cartsMs/cart/domain/cartCreatedDomainEvent";
 import CartCount from "../../../../../../contexts/cartsMs/cart/domain/valueObject/cartCount";
+import CommandBus from "../../../../../../contexts/shared/domain/commandBus/commandBus";
 import InvalidArgumentError from "../../../../../../contexts/shared/domain/invalidArgumentError";
 import Uuid from "../../../../../../contexts/shared/domain/valueObject/uuid";
+import CommandHandlersMapper from "../../../../../../contexts/shared/infrastructure/commandBus/commandHandlersMapper";
+import InMemoryCommandBus from "../../../../../../contexts/shared/infrastructure/commandBus/inMemoryCommandBus";
 import EventBusMock from "../../../shared/__mocks__/eventBusMock";
 import CartRepositoryMock from "../../__mocks__/cartRepositoryMock";
 
 describe("CartCreate Test Suit", () => {
   let cartRepository: CartRepositoryMock;
   let eventBus: EventBusMock;
-  let service: CartCreate;
+  let commandBus: CommandBus;
   beforeAll(() => {
     cartRepository = new CartRepositoryMock();
     eventBus = new EventBusMock();
-    service = new CartCreate(cartRepository, eventBus);
+    const service = new CartCreate(cartRepository, eventBus);
+    const commandHandler = new CartCreateCommandHandler(service);
+    const commandHandlersMapper = new CommandHandlersMapper([commandHandler]);
+    commandBus = new InMemoryCommandBus(commandHandlersMapper);
   });
   it("Should rise on Invalid Cart Id", async () => {
     // Given
+    const command = new CartCreateCommand("NonUUID", Uuid.random().toString());
     // When/Then
-    await expect(
-      service.run("NonUUID", Uuid.random().toString())
-    ).rejects.toThrow(InvalidArgumentError);
+    await expect(commandBus.dispatch(command)).rejects.toThrow(
+      InvalidArgumentError
+    );
   });
 
   it("Should rise on cart already exists.", async () => {
@@ -30,8 +39,9 @@ describe("CartCreate Test Suit", () => {
     const cartIdInUse = Uuid.random().toString();
     const userId = Uuid.random().toString();
     cartRepository.whenCountByIdReturn(new CartCount(1));
+    const command = new CartCreateCommand(cartIdInUse, userId);
     // When/Then
-    await expect(service.run(cartIdInUse, userId)).rejects.toThrow(
+    await expect(commandBus.dispatch(command)).rejects.toThrow(
       CartAlreadyExists
     );
   });
@@ -41,8 +51,9 @@ describe("CartCreate Test Suit", () => {
     const cartId = Uuid.random();
     const userId = Uuid.random();
     cartRepository.whenCountByIdReturn(new CartCount(0));
+    const command = new CartCreateCommand(cartId.toString(), userId.toString());
     // When
-    await service.run(cartId.toString(), userId.toString());
+    await commandBus.dispatch(command);
     const lastSavedCart = cartRepository.lastSavedCart();
     // Then
     expect(lastSavedCart).toBeInstanceOf(Cart);
